a0cf705cb28bb770cb87aa0dc6421f8a5691eeb1
PURVIK-31 <mepurviksharma@gmail.com>
2025-01-04 04:32:18 +0530

    db next

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controllers.js";
 const router = express.Router();
-router.post("/signup", );
-router.post("/login", ;
-router.post("/logout",);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
