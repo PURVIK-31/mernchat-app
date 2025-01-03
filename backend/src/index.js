d1a697a75f2917aa47365fef8e0aaa89dc05c0e8
PURVIK-31 <mepurviksharma@gmail.com>
2025-01-04 04:28:57 +0530

    controllers init

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 const app = express();
 
-app.use("/api/auth, authRoutes");
+app.use("/api/auth", authRoutes);
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
