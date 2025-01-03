8b3ab7d1f4d95eafb536f73d300ef64c65ad56c5
PURVIK-31 <mepurviksharma@gmail.com>
2025-01-04 05:00:16 +0530

    checking pass and finone

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,8 +1,13 @@
+import User from "../models/user.models.js";
 export const signup = (req, res) => {
   const { email, fullName, password } = req.body;
   try {
     if (password.length < 6) {
       res.status(400).send("Password must be at least 6 characters long");
+    }
+    const user = new User.findOne({ email });
+    if (user) {
+      res.status(400).send("Email already exists");
     }
   } catch (error) {}
 };
