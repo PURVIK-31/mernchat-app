0e9d45d88a449b7164186d236da0a9c2f39016a6
PURVIK-31 <mepurviksharma@gmail.com>
2025-01-04 07:34:09 +0530

    done for the day

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieparser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import dotenv from "dotenv";
 dotenv.config();
@@ -9,7 +10,9 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT;
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 app.listen(PORT, () => {
   console.log("Server started on port : ", PORT);
   connectDB();
