3a594b78f892ad4916603768669bb6981023b25a
PURVIK-31 <mepurviksharma@gmail.com>
2025-01-04 07:15:38 +0530

    done routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieparser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieparser());
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes);
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controllers.js";
+import { protectroute } from "../middleware/auth.middleware.js";
+import {
+  login,
+  logout,
+  signup,
+  updateProfile,
+  checkAuth,
+} from "../controllers/auth.controllers.js";
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-
+router.put("/updateProfile", protectroute, updateProfile);
+router.get("/checkAuth", protectroute, checkAuth);
 export default router;
