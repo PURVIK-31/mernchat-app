505f26930f47e72cb7883067e9bd6d2e789a3d9d
PURVIK-31 <mepurviksharma@gmail.com>
2025-01-05 22:16:28 +0530

    signuppage

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 const SignupPage = () => {
   const [showpassword, setShowpassword] = useState(false);
+  const [formdata, setFormdata] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
   return <div>SignupPage</div>;
 };
 
