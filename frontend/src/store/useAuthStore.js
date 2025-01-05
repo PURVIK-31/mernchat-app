d3ad5e663f2b295c332c6b67a67d9248f766d844
PURVIK-31 <mepurviksharma@gmail.com>
2025-01-05 17:29:03 +0530

    authstore

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,1 +1,12 @@
-import zustand from "zustand";
+import { create } from "zustand";
+export const useAuthstore = create((set) => ({
+  authUser: null,
+  isSigningup: false,
+  isLoginnging: false,
+  isUpdatingprofile: false,
+  isCheckingAuth: true,
+  checkAuth: async () => {
+    try {
+    } catch (error) {}
+  },
+}));
