dedb1c43fdfb253a32dd846c411a6675b79fea34
PURVIK-31 <mepurviksharma@gmail.com>
2025-01-05 17:12:38 +0530

    init the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 }
